28cc860d7bb3922e7f213126c654136f9b39801f
Johan Rade <johan.rade@gmail.com>
2022-01-09 16:52:00 +0100

    Updated copyright notice

--- Cpp/JrBoostLib/BaseOptions.h
+++ Cpp/JrBoostLib/BaseOptions.h
@@ -1,4 +1,4 @@
-//  Copyright 2021 Johan Rade <johan.rade@gmail.com>.
+//  Copyright 2022 Johan Rade <johan.rade@gmail.com>.
 //  Distributed under the MIT license.
 //  (See accompanying file License.txt or copy at https://opensource.org/licenses/MIT)
 
